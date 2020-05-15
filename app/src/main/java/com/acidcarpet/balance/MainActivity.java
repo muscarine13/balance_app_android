05f8e87001470e5e3c6bbda342326d3b05b3f72f
acidmors <zealot66613@gmail.com>
2020-05-15 20:36:09 +0300

    ***0.0.3*** Сделали нормальные тосты, Сделали сохранение и загрузку.

--- app/src/main/java/com/acidcarpet/balance/MainActivity.java
+++ app/src/main/java/com/acidcarpet/balance/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.RequiresApi;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -16,19 +17,29 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String FILENAME = "storage";
+    private static final String LOG_TAG = "MAIN_ACTIVITY";
+
     NumberFormat formatter = new DecimalFormat("#0.00");
 
-
+    private Toast toast;
     private Button good_button;
     private Button bad_button;
     private TextView good_percent_label;
     private TextView bad_percent_label;
     private TextView motivation_label;
     private ProgressBar balance_bar;
+
 
 
     @Override
@@ -56,14 +67,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
+
+        load();
+
         good_button = (Button) findViewById(R.id.good_button);
         bad_button = (Button) findViewById(R.id.bad_button);
         good_percent_label = (TextView) findViewById(R.id.good_label);
         bad_percent_label = (TextView) findViewById(R.id.bad_label);
         motivation_label = (TextView) findViewById(R.id.motivation_label);
         balance_bar = (ProgressBar) findViewById(R.id.balance_bar);
-
-
 
         good_button.setOnClickListener(new View.OnClickListener() {
 
@@ -80,29 +93,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
         refresh();
     }
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //load();
 
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        save();
+    }
 
     private void good_tap(){
 
         Storage.get().add_good_tap();
         refresh();
-        Toast.makeText(MainActivity.this,
-                R.string.good_toast,
-                Toast.LENGTH_SHORT).show();
+        toast.setText(R.string.good_toast);
+        toast.show();
     }
-
     private void bad_tap(){
 
         Storage.get().add_bad_tap();
         refresh();
-        Toast.makeText(MainActivity.this,
-                R.string.bad_toast,
-                Toast.LENGTH_SHORT).show();
+        toast.setText(R.string.bad_toast);
+        toast.show();
     }
 
 
@@ -116,5 +138,51 @@ public class MainActivity extends AppCompatActivity {
 
         balance_bar.setProgress((int)(Storage.get().get_bad_percent()*1000)-1);
 
+    }
+
+    boolean save() {
+        try {
+            // отрываем поток для записи
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+                    openFileOutput(FILENAME, MODE_PRIVATE)));
+            // пишем данные
+            bw.write(Storage.get().serial());
+            // закрываем поток
+            bw.close();
+            Log.d(LOG_TAG, "Файл записан");
+            toast.setText("Сохранено!");
+            toast.show();
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    boolean load() {
+        try {
+            // открываем поток для чтения
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    openFileInput(FILENAME)));
+            String str = "";
+            String out = "";
+            // читаем содержимое
+
+            while ((str = br.readLine()) != null) {
+                out+=str;
+            }
+            Storage.de_serial(out);
+            toast.setText("Загружено!");
+            toast.show();
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
--- app/src/main/java/com/acidcarpet/balance/Storage.java
+++ app/src/main/java/com/acidcarpet/balance/Storage.java
@@ -1,10 +1,31 @@
 package com.acidcarpet.balance;
 
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import static android.content.Context.MODE_PRIVATE;
+
+
 public class Storage {
+    private static final String LOG_TAG = "STORAGE";
+    private static final String FILENAME = "storage";
+
     private static Storage instance;
     public static Storage get(){
         if (instance==null) instance = new Storage();
         return instance;
+    }
+    public static void set(Storage storage){
+        instance = storage;
     }
 
     private Storage(){
@@ -37,5 +58,29 @@ public class Storage {
     public double get_bad_percent(){
         return (double)bad_taps/((double)bad_taps+(double)good_taps);
     }
+    
+   public String serial(){
+        String out;
+        out=
+        good_taps+","+
+                bad_taps;
+
+        return out;
+   }
+   public static boolean de_serial(String file){
+        try{
+            String[] array = file.split(",");
+            int good_taps = Integer.parseInt(array[0]);
+            int bad_taps = Integer.parseInt(array[1]);
+            if(instance==null) instance = new Storage();
+            instance.good_taps = good_taps;
+            instance.bad_taps = bad_taps;
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+   }
+
+
 
 }

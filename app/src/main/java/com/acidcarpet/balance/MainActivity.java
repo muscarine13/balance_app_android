c809430a875ad8838f906dfe3fb5542ab824e213
acidmors <zealot66613@gmail.com>
2020-05-15 15:11:00 +0300

    ***0.0.1*** Первый коммит. Работает, но ничего не сохраняет. Оформление пока стандартное.

---
+++ app/src/main/java/com/acidcarpet/balance/MainActivity.java
@@ -0,0 +1,83 @@
+package com.acidcarpet.balance;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+    NumberFormat formatter = new DecimalFormat("#0.00");
+
+
+    private Button good_button;
+    private Button bad_button;
+    private TextView good_percent_label;
+    private TextView bad_percent_label;
+    private TextView motivation_label;
+    private ProgressBar balance_bar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        good_button = (Button) findViewById(R.id.good_button);
+        bad_button = (Button) findViewById(R.id.bad_button);
+        good_percent_label = (TextView) findViewById(R.id.good_label);
+        bad_percent_label = (TextView) findViewById(R.id.bad_label);
+        motivation_label = (TextView) findViewById(R.id.motivation_label);
+        balance_bar = (ProgressBar) findViewById(R.id.balance_bar);
+
+
+        good_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                good_tap();
+            }
+        });
+        bad_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                bad_tap();
+            }
+        });
+
+    }
+
+
+
+    private void good_tap(){
+
+        Storage.get().add_good_tap();
+        refresh();
+    }
+
+    private void bad_tap(){
+
+        Storage.get().add_bad_tap();
+        refresh();
+    }
+
+
+
+    private void refresh(){
+
+        good_percent_label.setText(formatter.format(Storage.get().get_good_percent()*100)+"%");
+        bad_percent_label.setText(formatter.format(Storage.get().get_bad_percent()*100)+"%");
+
+        motivation_label.setText("Курочка по зернышку клюёт, а двор весь в помете. \nРусский народ, неизвестный год.");
+
+        balance_bar.setProgress((int)(Storage.get().get_bad_percent()*1000)-1);
+
+    }
+}
---
+++ app/src/main/java/com/acidcarpet/balance/Storage.java
@@ -0,0 +1,41 @@
+package com.acidcarpet.balance;
+
+public class Storage {
+    private static Storage instance;
+    public static Storage get(){
+        if (instance==null) instance = new Storage();
+        return instance;
+    }
+
+    private Storage(){
+        good_taps= 0;
+        bad_taps = 0;
+    }
+    private int good_taps;
+    private int bad_taps;
+
+    public void add_good_tap(){
+        good_taps++;
+    }
+    public void add_bad_tap(){
+        bad_taps++;
+    }
+
+    public double get_sum(){
+        return good_taps+bad_taps;
+    }
+    public int get_good_taps() {
+        return good_taps;
+    }
+    public int get_bad_taps() {
+        return bad_taps;
+    }
+
+    public double get_good_percent(){
+        return (double)good_taps/((double)good_taps+(double)bad_taps);
+    }
+    public double get_bad_percent(){
+        return (double)bad_taps/((double)bad_taps+(double)good_taps);
+    }
+
+}
